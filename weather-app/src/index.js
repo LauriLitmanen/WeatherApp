6de98ab8c9f65b0ece482d24b4172e1c1ce40ca0
LauriLitmanen <litmanen.lauri@gmail.com>
2020-04-10 13:00:34 +0300

    App & Card components with dummy data, basic styling

--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+
+
+function Card(props) {
+    return (
+        <button className="card">
+            <p>Pvm</p>
+            <img src="http://openweathermap.org/img/wn/10d@2x.png"></img>
+            <div className="temp">
+                <p className="temp-high">25</p>
+                <p className="temp-low">15</p>                
+            </div>
+        </button>
+    )
+}
+
+function App() {
+    return(
+        <div className="wrapper">
+            <Card></Card>
+        </div>
+    );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <App></App>,
+    document.getElementById('root')
+  );
