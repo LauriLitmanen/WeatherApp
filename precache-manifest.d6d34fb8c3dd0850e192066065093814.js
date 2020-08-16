e72069e64cac396b777b12442f80dcc4a1e70ced
LauriLitmanen <litmanen.lauri@gmail.com>
2020-08-16 15:41:42 +0300

    Updates

---
+++ precache-manifest.d6d34fb8c3dd0850e192066065093814.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42073ab028856cf9c384ac57f00da15e",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "8effde548f645b5ff1ea",
+    "url": "/WeatherApp/static/css/main.6dfbd595.chunk.css"
+  },
+  {
+    "revision": "d9a9b698d38fa67c1121",
+    "url": "/WeatherApp/static/js/2.7a91ffbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp/static/js/2.7a91ffbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8effde548f645b5ff1ea",
+    "url": "/WeatherApp/static/js/main.b40d48b6.chunk.js"
+  },
+  {
+    "revision": "f34a18f0592fbbc8ce7e",
+    "url": "/WeatherApp/static/js/runtime-main.dcd8b410.js"
+  }
+]);
